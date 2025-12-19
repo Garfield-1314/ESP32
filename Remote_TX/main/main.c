f31c1864504076fe044b0f7f47344c10b33609ce
Garfield-1314 <447972538@qq.com>
2025-12-19 17:58:22 +0800

    lvgl更改为8.4.0版本

--- Remote_TX/main/main.c
+++ Remote_TX/main/main.c
@@ -1,11 +1,12 @@
 #include "include.h"
 
+gt911_dev_t gt911_dev;
 
 static void user_component_init(void){
     tusb_serial_init();
     vTaskDelay(pdMS_TO_TICKS(1000));
     lcd_st7789_init();
-    gt911_init();
+    gt911_init_default(&gt911_dev);
     user_lvgl_init();
     ui_init();
 }
--- Remote_TX/main/src/user_lvgl.c
+++ Remote_TX/main/src/user_lvgl.c
@@ -4,23 +4,9 @@
 #include "lvgl.h"
 #include "esp_timer.h"
 
-static const char *TAG = "user_lvgl";
-
-static void lv_tick_task(void *arg) {
-    (void) arg;
-    lv_tick_inc(1);
-}
+static const char *TAG = "lvgl";
 
 void user_lvgl_init(void) {
-    ESP_LOGI(TAG, "Initializing User LVGL");
 
     lv_init();
-
-    const esp_timer_create_args_t periodic_timer_args = {
-        .callback = &lv_tick_task,
-        .name = "periodic_gui"
-    };
-    esp_timer_handle_t periodic_timer;
-    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
-    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 1000));
 }
