ea529a34c5e61cd795fa050589f9872a528ec3b1
Garfield-1314 <447972538@qq.com>
2026-04-22 16:59:39 +0800

    update

--- Remote_TX/main/driver/inc/user_lvgl.h
+++
@@ -1,8 +0,0 @@
-#ifndef __USER_LVGL_H
-#define __USER_LVGL_H
-
-#include "lvgl.h"
-
-void user_lvgl_init(void);
-
-#endif // __USER_LVGL_H
--- Remote_TX/main/ui/inc/app_page.h
+++
@@ -1,9 +0,0 @@
-#ifndef __APP_PAGE_H
-#define __APP_PAGE_H
-
-#include "lvgl.h"
-
-extern lv_obj_t *app_page;
-lv_obj_t * create_app_page(void);
-
-#endif
