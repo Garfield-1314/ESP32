112e8c16b372310a82749ce238be5da5866e9a17
Garfield-1314 <447972538.com>
2026-06-22 16:23:11 +0800

    rm example

--- example/lsm6dsm_example/main/tusb_serial.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/**
- * @brief Initialize USB CDC-ACM (virtual serial port) for debugging output.
- *        Redirects ESP-IDF log output (ESP_LOGI, etc.) to the USB serial port.
- */
-void tusb_serial_init(void);
